09ce4dd25a8e549cfb6b33355b2611b83460491f
christopher william <christopherwilliam.bs@gmail.com>
2020-09-08 08:23:30 -0300

    docs: pages files create

---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+import Home from "./home";
+import Login from "./login";
+import Profile from "./profile";
+import Register from "./register";
+import Shelves from "./shelves";
+import Timeline from "./timeline";
+
+export { Home, Login, Profile, Register, Shelves, Timeline };
---
+++ src/pages/shelves/index.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Shelves() {
+  return <div>Shelves</div>;
+}
