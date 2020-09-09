3797647c950028c4eceb54f8efe57e254d307ba2
christopher william <christopherwilliam.bs@gmail.com>
2020-09-09 05:04:43 -0300

    feat: create condicional render to set hastoken to true/false and push user to timenine/login

--- src/components/menu/index.jsx
+++ src/components/menu/index.jsx
@@ -1,29 +1,50 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Link, useHistory } from "react-router-dom";
 import { StyledHeader, StyledMenu } from "./styledmenu";
 
 export default function Menu() {
+  const history = useHistory();
+  const [hasToken, setHasToken] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      setHasToken(true);
+      history.push("/timeline");
+    } else {
+      history.push("/login");
+    }
+  }, [history]);
+
   return (
     <StyledHeader>
       <StyledMenu>
         <h1>BookBook</h1>
-        <StyledMenu.Section>
-          <StyledMenu.item>
-            <Link to="/login">Login</Link>
-          </StyledMenu.item>
-          <StyledMenu.item>
-            <Link to="/profile">Profile</Link>
-          </StyledMenu.item>
-          <StyledMenu.item>
-            <Link to="/register">Register</Link>
-          </StyledMenu.item>
-          <StyledMenu.item>
-            <Link to="/shelves">Shelves</Link>
-          </StyledMenu.item>
-          <StyledMenu.item>
-            <Link to="/timeline">Timeline</Link>
-          </StyledMenu.item>
-        </StyledMenu.Section>
+        {hasToken ? (
+          <StyledMenu.Section>
+            <StyledMenu.item>
+              <Link to="/profile">Profile</Link>
+            </StyledMenu.item>
+
+            <StyledMenu.item>
+              <Link to="/shelves">Shelves</Link>
+            </StyledMenu.item>
+
+            <StyledMenu.item>
+              <Link to="/timeline">Timeline</Link>
+            </StyledMenu.item>
+          </StyledMenu.Section>
+        ) : (
+          <StyledMenu.Section>
+            <StyledMenu.item>
+              <Link to="/login">Login</Link>
+            </StyledMenu.item>
+
+            <StyledMenu.item>
+              <Link to="/register">Register</Link>
+            </StyledMenu.item>
+          </StyledMenu.Section>
+        )}
       </StyledMenu>
     </StyledHeader>
   );
