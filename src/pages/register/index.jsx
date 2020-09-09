807e92765e17a2b14d352b81739db1ee228ac2d4
Ricardo <rsdsantis@gmail.com>
2020-09-09 16:26:40 -0300

    Feat: add form, input validations and message info

--- src/pages/register/index.jsx
+++ src/pages/register/index.jsx
@@ -1,14 +1,14 @@
-import React, { useState } from "react";
+import React from "react";
 import { message } from "antd";
+import axios from "axios";
 import {
-  Container,
-  LoginContainer,
+  InputContainer,
   StyledForm,
   H2Form,
   StyledButton,
   LinkA,
-} from "./Register.js";
-import StylezedInput from "../../components/input/Input.jsx";
+  StyledInput,
+} from "./register.js";
 import { useHistory } from "react-router-dom";
 
 const Register = () => {
@@ -20,110 +20,114 @@ const Register = () => {
 
   const onFinish = (values) => {
     let apiObject = { user: values };
-    apiObject.user.image_url = values.image_url.value;
-    api
-      .post("users", apiObject)
+
+    axios
+      .post("https://ka-users-api.herokuapp.com/users", apiObject)
       .then((res) => {
         info("Register successful");
         history.push("/");
       })
-      .catch((res) => {
-        info("Register Failed");
+      .catch((err) => {
+        info("Register Failed, please verify your inputs");
       });
   };
 
   return (
     <>
-      <Container>
-        <LoginContainer>
-          <StyledForm onFinish={onFinish}>
-            <H2Form>Join us!</H2Form>
+      <InputContainer>
+        <StyledForm onFinish={onFinish}>
+          <H2Form>Join us!</H2Form>
+          <StyledForm.Item
+            style={{ color: "white" }}
+            name="name"
+            label="Name"
+            type="text"
+            rules={[
+              { required: true, message: "Please type your name!" },
+              {
+                pattern: /[A-Za-z]* [A-Za-z]*/,
+                message: "Your name must be formated as 'name last name')",
+              },
+            ]}
+          >
+            <StyledInput />
+          </StyledForm.Item>
+          <StyledForm.Item
+            name="user"
+            label="Username"
+            type="text"
+            rules={[
+              { required: true, message: "Please input your username!" },
+              {
+                min: 6,
+                message: "Username must be at least 6 characters long",
+              },
+            ]}
+          >
+            <StyledInput />
+          </StyledForm.Item>
 
-            <StylezedInput
-              icon="fas fa-user"
-              name="name"
-              label="Name"
-              type="text"
-              rules={[
-                { required: true, message: "Please type your name!" },
-                {
-                  pattern: /[A-Z][a-z]* [A-Z][a-z]*/,
-                  message: "Your name must be (Name Last-name)",
+          <StyledForm.Item
+            name="email"
+            label="Email"
+            type="text"
+            rules={[
+              { required: true, message: "Please type your Email!" },
+              {
+                pattern: /^[a-zA-Z0-9_-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/gi,
+                message: "Email must be example@example.com",
+              },
+            ]}
+          >
+            <StyledInput />
+          </StyledForm.Item>
+
+          <StyledForm.Item
+            name="password"
+            label="Password"
+            type="password"
+            rules={[
+              { required: true, message: "Please input your password!" },
+              {
+                min: 6,
+                message: "Password must be at least 6 characters long",
+              },
+              {
+                pattern: /(?=.*[}{,^?~=+\-_*\-+|!@#$%&-+¨´"'])/,
+                message: "Must contain at least one special character",
+              },
+            ]}
+          >
+            <StyledInput />
+          </StyledForm.Item>
+
+          <StyledForm.Item
+            name="confirmPassword"
+            label="Confirm password"
+            type="password"
+            rules={[
+              { required: true, message: "Please confirm your password!" },
+              ({ getFieldValue }) => ({
+                validator(rule, value) {
+                  if (!value || getFieldValue("password") === value) {
+                    return Promise.resolve();
+                  }
+                  return Promise.reject(
+                    "Passwords that you entered do not match!"
+                  );
                 },
-              ]}
-            />
-            <StylezedInput
-              icon="fas fa-user"
-              name="user"
-              label="Username"
-              type="text"
-              rules={[
-                { required: true, message: "Please input your username!" },
-                {
-                  min: 6,
-                  message: "Username must be at least 6 characters long",
-                },
-              ]}
-            />
-            <StylezedInput
-              icon="fas fa-lock"
-              name="email"
-              label="Email"
-              type="text"
-              rules={[
-                { required: true, message: "Please type your Email!" },
-                {
-                  pattern: /^[a-zA-Z0-9_-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/gi,
-                  message: "Email must be example@example.com",
-                },
-              ]}
-            />
-            <StylezedInput
-              icon="fas fa-lock"
-              name="password"
-              label="Password"
-              type="password"
-              rules={[
-                { required: true, message: "Please input your password!" },
-                {
-                  min: 6,
-                  message: "Password must be at least 6 characters long",
-                },
-                {
-                  pattern: /(?=.*[}{,^?~=+\-_*\-+|!@#$%&-+¨´"'])/,
-                  message: "Must contain at least one special character",
-                },
-              ]}
-            />
-            <StylezedInput
-              icon="fas fa-lock"
-              name="confirmPassword"
-              label="Confirm password"
-              type="password"
-              rules={[
-                { required: true, message: "Please confirm your password!" },
-                ({ getFieldValue }) => ({
-                  validator(rule, value) {
-                    if (!value || getFieldValue("password") === value) {
-                      return Promise.resolve();
-                    }
-                    return Promise.reject(
-                      "Passwords that you entered do not match!"
-                    );
-                  },
-                }),
-              ]}
-            />
-            <LinkA to="/">
-              {" "}
-              <i className="fas fa-sign-out-alt"></i>Voltar
-            </LinkA>
-            <StyledButton type="submit" htmlType="submit">
-              Register
-            </StyledButton>
-          </StyledForm>
-        </LoginContainer>
-      </Container>
+              }),
+            ]}
+          >
+            <StyledInput />
+          </StyledForm.Item>
+
+          <LinkA to="/">Voltar</LinkA>
+          <StyledButton type="submit" htmlType="submit">
+            Register
+          </StyledButton>
+        </StyledForm>
+      </InputContainer>
     </>
   );
 };
--- src/pages/register/register.js
+++ src/pages/register/register.js
@@ -1,42 +1,30 @@
 import styled from "styled-components";
-import { Form, Button } from "antd";
+import { Form, Button, Input } from "antd";
 import { Link } from "react-router-dom";
 
-export const Container = styled.div`
-  width: 100vw;
-  height: 100vh;
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  grid-gap: 7rem;
-  padding: 0 2rem;
-`;
-
-export const LoginContainer = styled.div`
+export const InputContainer = styled.div`
   display: flex;
   align-items: center;
   text-align: center;
+`;
 
-  .ant-select-selector {
-    height: 55px !important;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    margin: 0 auto;
-  }
-  .ant-select-selection-item {
-    margin-top: 30px;
-  }
+export const StyledInput = styled(Input)`
+  margin: 1rem;
 `;
 
 export const StyledForm = styled(Form)`
   width: 360px;
+  display: flex;
+  align-items: center;
+  text-align: center;
+  flex-direction: column;
 `;
 
 export const H2Form = styled.h2`
   font-size: 2.9rem;
   text-transform: uppercase;
   margin: 15px 0;
-  color: #333;
+  color: #ccc;
 `;
 
 export const LinkA = styled(Link)`
@@ -66,6 +54,7 @@ export const StyledButton = styled(Button)`
   transition: 0.5s;
 
   &:hover {
+    background-color: #33c;
   }
 
   &:focus {
